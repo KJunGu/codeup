de7b1232da9453e48866bf7075a98bfe0307b1f6
KJunGu <rkdwnsrn5@naver.com>
2022-07-06 16:29:03 +0900

    7/6 update

---
+++ codeup3~11.c
@@ -0,0 +1,55 @@
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("Hello\nWorld");
+//     return 0;
+// }
+
+// -------------------------------code 4-------------------
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("\'Hello\'");
+//     return 0;
+// }
+
+// -------------------------------code 5--------------------
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("\"Hello World\"");
+//     return 0;
+// }
+// -------------------------------code 6--------------------
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("\"!@#$%%^&*()\"");
+//     return 0;
+// }
+// -------------------------------code 7--------------------
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("\"C:\\Download\\hello.cpp\""); // \를 입력할때는 \를 하나 더 써야한다
+//     return 0;
+// }  
+// -------------------------------code 8--------------------
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("\u250c\u252c\u2510\n\u251c\u253c\u2524\n\u2514\u2534\u2518");
+//     return 0;
+// }
+// -------------------------------code 9--------------------
+// -------------------------------code 10--------------------
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     printf("%d", n);
+//     return 0;
+// }
+// -------------------------------code 11--------------------
