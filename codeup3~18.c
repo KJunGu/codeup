f01414d17c432210fedc3fd979336ab54309ffb5
KJunGu <rkdwnsrn5@naver.com>
2022-07-07 21:05:24 +0900

    7/7 update

---
+++ codeup3~18.c
@@ -0,0 +1,117 @@
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("Hello\nWorld");
+//     return 0;
+// }
+
+// -------------------------------code 4-------------------
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("\'Hello\'");
+//     return 0;
+// }
+
+// -------------------------------code 5--------------------
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("\"Hello World\"");
+//     return 0;
+// }
+// -------------------------------code 6--------------------
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("\"!@#$%%^&*()\"");
+//     return 0;
+// }
+// -------------------------------code 7--------------------
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("\"C:\\Download\\hello.cpp\""); // \를 입력할때는 \를 하나 더 써야한다
+//     return 0;
+// }  
+// -------------------------------code 8--------------------
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("\u250c\u252c\u2510\n\u251c\u253c\u2524\n\u2514\u2534\u2518");
+//     return 0;
+// }
+// -------------------------------code 9--------------------
+// -------------------------------code 10--------------------
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     printf("%d", n);
+//     return 0;
+// }
+// -------------------------------code 11--------------------
+// #include <stdio.h>
+// int main(void)
+// {
+//     char x;
+//     scanf("%c", &x);
+//     printf("%c",x);
+//     return 0;
+// }
+// -------------------------------code 12--------------------
+// #include <stdio.h>
+// int main(void)
+// {
+//     float x;
+//     scanf("%f", &x);
+//     printf("%f",x);
+//     return 0;
+// }
+// -------------------------------code 13--------------------
+// #include <stdio.h>
+// int main(void)
+// {
+//     int x,y;
+//     scanf("%d%d" ,&x,&y);
+//     printf("%d %d",x,y);
+//     return 0;
+// }
+// -------------------------------code 14--------------------
+// #include <stdio.h>
+// int main(void)
+// {
+//     char x,y;
+//     scanf("%c %c",&x,&y);
+//     printf("%c %c",y,x);
+//     return 0;
+// }
+// -------------------------------code 15--------------------
+// #include <stdio.h>
+// int main()
+// {
+//     float x;
+//     scanf("%f",&x);
+//     printf("%.2f",x);
+//     return 0;
+// }
+// -------------------------------code 17--------------------
+// #include <stdio.h>
+// int main()
+// {
+//     int a;
+//     scanf("%d",&a);
+//     printf("%d %d %d",a,a,a);
+//     return 00;
+// }
+// -------------------------------code 18--------------------
+#include <stdio.h>
+int main(void)
+{
+    int x,y;
+    scanf("%d:%d",&x,&y);
+    printf("%d:%d",x,y);
+    return 0;
+}
